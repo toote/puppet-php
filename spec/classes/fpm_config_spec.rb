5de89b14009bea5e24da776ef7bbed8831e30a01
Franz Pletz <fpletz@fnordicwalking.de>
2014-07-30 19:12:32 +0200

    Fix specs for php::fpm::config

--- spec/classes/fpm_config_spec.rb
+++ spec/classes/fpm_config_spec.rb
@@ -1,33 +1,17 @@
 require 'spec_helper'
 
 describe 'php::fpm::config' do
-  let(:facts) { {  :osfamily => 'Debian',
-                   :path     => '/usr/local/bin:/usr/bin:/bin' } }
+  let(:facts) { { :osfamily => 'Debian' } }
 
-  context 'valid config' do
-    let(:title) { 'unique-name' }
+  context 'creates config file' do
     let(:params) {{
-      :file   => '/etc/php5/conf.d/unique-name.ini',
-      :config => [
-        'set .anon/apc.enabled 1'
-      ]
+      :config_file => '/etc/php5/conf.d/unique-name.ini',
     }}
 
-    it { should contain_php__fpm__config('unique-name').with({
-        'file'    => '/etc/php5/conf.d/unique-name.ini',
-        'config'  => ['set .anon/apc.enabled 1']
+    it { should contain_file('/etc/php5/conf.d/unique-name.ini').with({
+        'ensure' => 'present'
       })
     }
-  end
-
-  context 'invalid config (string)' do
-    let(:title) { 'unique-name' }
-    let(:params) {{
-      :file => '/etc/php5/conf.d/unique-name.ini',
-      :config => 'hello world'
-    }}
-
-    it { expect { should raise_error(Puppet::Error) }}
   end
 
 end
